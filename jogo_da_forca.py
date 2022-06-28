31cdbc539ce5c69ea6f15c4e0fe839b680fa48f2
Lucas Silva <lucas.silva@live.com.pt>
2022-06-28 07:38:02 +0100

    new

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,4 +1,3 @@
-from curses.ascii import isalpha
 import json
 import random 
 import pygame as pg
@@ -25,75 +24,87 @@ azul = (0, 0, 255)
 
 # Abrir uma janela
 
-pg.init()
 window = pg.display.set_mode((720, 480)) # Abre uma janela
-pg.display.set_caption('Jogo da Forca') # Altera o nome da janela para 'Jogo da forca'
+pg.font.init()
+font = pg.font.SysFont('Courier New', 50)
+font_rb = pg.font.SysFont('Courier New', 30)
 
 forca_img = pg.image.load('forca.png') # Carrega a imagem
 window.fill(branco)
 window.blit(forca_img, (50, 50))
-pg.draw.circle(window, preto, (156.5, 99), 30, 3) #cabeca
-pg.draw.line(window, preto, (156.5, 129), (156.5, 189), 5) #corpo
-pg.draw.line(window, preto, (156.5, 189), (186.5, 219), 5) #perna direita
-pg.draw.line(window, preto, (156.5, 189), (126.5, 219), 5) #perna esquerda
-pg.draw.line(window, preto, (156.5, 129), (186.5, 179), 5)
-pg.draw.line(window, preto, (156.5, 129), (126.5, 179), 5)
+pg.display.flip()
 
+alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-
-
-# pg.draw.line(window, preto, False, )
-
-while True:
-	pg.display.update()
-
-
-# alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
-# letra = random.choice(alfabeto)
-# choice = True
-# while choice:
-# 	choice = False
-# 	dicionario = make_request(letra)
-# 	palavra = dicionario["word"]
-# 	for c in palavra:
-# 		if not isalpha(c):
-# 			choice = True
+letra = random.choice(alfabeto)
+choice = True
+while choice:
+	choice = False
+	dicionario = make_request(letra) 
+	palavra = dicionario["word"]
+	if not palavra.isalpha():
+ 		choice = True
 
 
 # # lines =  400 / (dicionario["count"] * 2)
 
-# lifes = 5
-# correct_guesses = ''
-# flag = len(palavra)
-# print('lifes: ' + str(lifes))
+lifes = 0
+correct_guesses = [' ', '-']
+flag = len(palavra)
+print('lifes: 6')
 
-# while lifes > 0:
-# 	flag = len(palavra)
+def desenhar_restart(window):
+	window.fill(branco)
+	texto = font_rb.render('RESTART', True, preto)
+	forca_img = pg.image.load('forca.png') # Carrega a imagem
+	window.blit(forca_img, (50, 50))
+	window.blit(texto, (300, 350))
 
-# 	for char in palavra:
-# 		if char in correct_guesses:
-# 			flag -= 1
-# 			print(char)
-# 		else:
-# 			print('-')
-# 	if flag == 0:
-# 			print('Congrats! You win!')
-# 			break
+while True:
 
-# 	guess = input('Take your guesses: ')
+	desenhar_restart(window)
+	for event in pg.event.get():
+		if event.type == pg.QUIT:
+			pg.quit()
+			quit()
 
-# 	if guess not in correct_guesses:
-# 		if guess in palavra:
-# 			print('Correct guess')
-# 			correct_guesses += guess
-# 		else:
-# 			print('Wrong guess')
-# 			lifes -= 1
-# 			print('lifes: ' + str(lifes))
-# if lifes == 0:
-# 	print('You lose!')
-# 	print(f"The word was {palavra}")
+	flag = len(palavra)
+	for char in palavra:
+		if char in correct_guesses:
+			flag -= 1
+			print(char)
+		else:
+			print('#')
+	if flag == 0:
+			print('Congrats! You win!')
+			break
+
+	guess = input('Take your guesses: ')
+	if guess not in correct_guesses:
+		if guess in palavra:
+			print('Correct guess')
+			correct_guesses += guess
+		else:
+			print('Wrong guess')
+			lifes += 1
+			if lifes >= 1:
+				pg.draw.circle(window, preto, (156.5, 99), 30, 3) #cabeca
+			if lifes >= 2:
+				pg.draw.line(window, preto, (156.5, 129), (156.5, 189), 5) #corpo
+			if lifes >= 3:
+				pg.draw.line(window, preto, (156.5, 129), (186.5, 179), 5) #braço direito
+			if lifes >= 4:
+				pg.draw.line(window, preto, (156.5, 129), (126.5, 179), 5) #braço esquerdo
+			if lifes >= 5:
+				pg.draw.line(window, preto, (156.5, 189), (186.5, 219), 5) #perna direita
+			if lifes >= 6:
+				pg.draw.line(window, preto, (156.5, 189), (126.5, 219), 5) #perna esquerda
+			print('lifes: ' + str(6 - lifes))
+
+	pg.display.update()
+if lifes == 6:
+	print('You lose!')
+	print(f"The word was {palavra}")
 
 
 
